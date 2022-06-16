1c835d44094712d4bf574aa2532e89c3108bc3bc
AdBol05 <98588523+AdBol05@users.noreply.github.com>
2022-06-16 13:33:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ let file = fs.readFileSync(directory).toString("utf-8");
 
 console.log("processing...");
 
-file = file.replace("Compass(raw)", "Compass X,Compass Y,Compass Z");
-file = file.replace("Accelerometer", "Acc Roll,Acc Pitch,AccYyaw");
-file = file.replace("Accelerometer(raw)", "AccRaw X,AccRaw Y,AccRaw Z");
-file = file.replace("Gyro", "Gyro Roll,Gyro Pitch,Gyro Yaw");
-file = file.replace("Gyro(raw)", "GyroRaw X,GyroRaw Y,GyroRaw Z");
-file = file.replace("Orintation", "Orientation Roll,Oricaentation Pitch,Orientation Yaw");
+file = file.replace("Compass(raw)", " Compass X, Compass Y, Compass Z");
+file = file.replace("Accelerometer", " Acc Roll, Acc Pitch, AccYyaw");
+file = file.replace("Accelerometer(raw)", " AccRaw X, AccRaw Y, AccRaw Z");
+file = file.replace("Gyro", " Gyro Roll, Gyro Pitch, Gyro Yaw");
+file = file.replace("Gyro(raw)", " GyroRaw X, GyroRaw Y, GyroRaw Z");
+file = file.replace("Orintation", " Orientation Roll, Oricaentation Pitch, Orientation Yaw");
 
-file = file.replace("coordinates", "Latitude,Longitude,Elevation");
-file = file.replace("magnetometer", "Magnetometer X,Magnetometer Y,Magnetometer Z");
+file = file.replace("coordinates", " Latitude, Longitude, Elevation");
+file = file.replace("magnetometer", " Magnetometer X, Magnetometer Y, Magnetometer Z");
 
 file = file.replaceAll("\"\{'x': ", "");
 file = file.replaceAll(" 'y': ", "");
