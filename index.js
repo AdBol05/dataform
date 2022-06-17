3420b9b9d41e265319cbdb51d9e4853938c03b1d
AdBol05 <98588523+AdBol05@users.noreply.github.com>
2022-06-17 20:59:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,59 +7,60 @@
 const fs = require("fs");
 const path = require("path");
 
+//resolve directory from argument
 const args = process.argv.slice(2);
 const directory = args[0];
 
-console.log('\x1b[32m%s\x1b[0m',"    ____        __        ____                   ");
-console.log('\x1b[32m%s\x1b[0m',"   / __ \\____ _/ /_____ _/ __/___  _________ ___ ");
-console.log('\x1b[32m%s\x1b[0m',"  / / / / __ `/ __/ __ `/ /_/ __ \\/ ___/ __ `__ \\");
-console.log('\x1b[32m%s\x1b[0m'," / /_/ / /_/ / /_/ /_/ / __/ /_/ / /  / / / / / /");
-console.log('\x1b[32m%s\x1b[0m',"/_____/\\__,_/\\__/\\__,_/_/  \\____/_/  /_/ /_/ /_/ ");
+//welcome screen
+console.log('\x1b[32m%s\x1b[0m',"    ____        __        ____                        ");
+console.log('\x1b[32m%s\x1b[0m',"   / __ \\____ _/ /_____ _/ __/___  _________ ___     ");
+console.log('\x1b[32m%s\x1b[0m',"  / / / / __ `/ __/ __ `/ /_/ __ \\/ ___/ __ `__ \\   ");
+console.log('\x1b[32m%s\x1b[0m'," / /_/ / /_/ / /_/ /_/ / __/ /_/ / /  / / / / / /     ");
+console.log('\x1b[32m%s\x1b[0m',"/_____/\\__,_/\\__/\\__,_/_/  \\____/_/  /_/ /_/ /_/  ");
 console.log("\n");
 
+//error handling
 if(directory == undefined){
         console.error('\x1b[31m%s\x1b[0m',"ERROR: CSV file path not provided");
         console.error('\x1b[31m%s\x1b[0m',"Please input CSV file path as an argument.");
-        process.exit(9); //invalid argument exit code
-}
+        process.exit(9);}
 
-if (fs.existsSync(directory)){
-        console.log('reading file on path: %s', directory);
-        let file = fs.readFileSync(directory).toString("utf-8");
-        console.log("processing...");
-
-        file = file.replace("date,", " Date/time,");
-        file = file.replace("Compass(raw)", "Compass X, Compass Y, Compass Z");
-        file = file.replace("Accelerometer", "Acc Roll, Acc Pitch, Acc Yaw");
-        file = file.replace("Accelerometer(raw)", "AccRaw X, AccRaw Y, AccRaw Z");
-        file = file.replace("Gyro", "Gyro Roll, Gyro Pitch, Gyro Yaw");
-        file = file.replace("Gyro(raw)", "GyroRaw X, GyroRaw Y, GyroRaw Z");
-        file = file.replace("Orintation", "Orientation Roll, Orientation Pitch, Orientation Yaw");
-        file = file.replace("coordinates", " Latitude, Longitude, Elevation");
-        file = file.replace("magnetometer", " Magnetometer X, Magnetometer Y, Magnetometer Z");
-
-        file = file.replaceAll("\"\{'x': ", "");
-        file = file.replaceAll(" 'y': ", "");
-        file = file.replaceAll(" 'z': ", "");
-        file = file.replaceAll("\"\{", "");
-        file = file.replaceAll("\}\"", "");
-        file = file.replaceAll("'roll': ", "");
-        file = file.replaceAll(" 'pitch': ", "");
-        file = file.replaceAll(" 'yaw': ", "");
-        file = file.replaceAll("IERS2010 latitude ", "");
-        file = file.replaceAll(" longitude ", ",");
-        file = file.replaceAll(" elevation ", ",");
-        file = file.replaceAll(" m,", ",");
-
-        fs.writeFileSync("dataExport.csv", file);
-        console.log("data written to: %s/dataExport.csv", path.dirname(__filename));
-        process.exit(0);
-}
-
-else{
+if (!fs.existsSync(directory)){
         console.error('\x1b[31m%s\x1b[0m',"ERROR: No such file or directory");
-        process.exit(9); //invalid argument exit code
-}
+        process.exit(9);}
+
+//file processing
+console.log('reading file on path: %s', directory);
+let file = fs.readFileSync(directory).toString("utf-8");
+console.log("processing...");
+
+file = file.replace("date,", " Date/time,");
+file = file.replace("Compass(raw)", "Compass X, Compass Y, Compass Z");
+file = file.replace("Accelerometer", "Acc Roll, Acc Pitch, Acc Yaw");
+file = file.replace("Accelerometer(raw)", "AccRaw X, AccRaw Y, AccRaw Z");
+file = file.replace("Gyro", "Gyro Roll, Gyro Pitch, Gyro Yaw");
+file = file.replace("Gyro(raw)", "GyroRaw X, GyroRaw Y, GyroRaw Z");
+file = file.replace("Orintation", "Orientation Roll, Orientation Pitch, Orientation Yaw");
+file = file.replace("coordinates", " Latitude, Longitude, Elevation");
+file = file.replace("magnetometer", " Magnetometer X, Magnetometer Y, Magnetometer Z");
+
+file = file.replaceAll("\"\{'x': ", "");
+file = file.replaceAll(" 'y': ", "");
+file = file.replaceAll(" 'z': ", "");
+file = file.replaceAll("\"\{", "");
+file = file.replaceAll("\}\"", "");
+file = file.replaceAll("'roll': ", "");
+file = file.replaceAll(" 'pitch': ", "");
+file = file.replaceAll(" 'yaw': ", "");
+file = file.replaceAll("IERS2010 latitude ", "");
+file = file.replaceAll(" longitude ", ",");
+file = file.replaceAll(" elevation ", ",");
+file = file.replaceAll(" m,", ",");
+
+//data output
+fs.writeFileSync("dataExport.csv", file);
+console.log("data written to: %s/dataExport.csv", path.dirname(__filename));
+process.exit(0);
 
 /*
 After the file has been cleaned, the output is written to dataExport.csv file.
